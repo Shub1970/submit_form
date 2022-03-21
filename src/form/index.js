5edd14ed4e98c810dff5fb34ab32725a761ec39e
shubham kumar <18shubham1998@gmail.com>
2022-03-21 19:10:26 +0530

    initial commit

---
+++ src/form/index.js
@@ -0,0 +1,41 @@
+import React from "react";
+import { useState } from "react";
+import {data} from "../data"
+// import Model from "./model";
+
+const Index=()=>{
+    const [name,setName]=useState('')
+    const handleSubmit=(e)=>{
+        e.preventdefault();
+        setName(e.value);
+    }
+    return(
+        <>
+       
+    <form onSubmit={handleSubmit} className="form">
+        <div>
+        <input 
+        type="text" 
+        value={name}
+        onChange={(e)=>{setName(e.target.value)}}
+        />
+        </div>
+        <button className="btn">add</button>
+    </form>
+    {data.map((person)=>{
+        return(
+            <div key={person.id} className="item">
+                <h4>{person.name}</h4>
+                <div>
+                <button className="btn">remove</button>
+                </div>
+            </div>
+        )
+    })}
+    
+
+    
+        </>
+    )
+}
+export default Index;
---
+++ src/form/model.js
@@ -0,0 +1,6 @@
+import React from "react";
+const Model=()=>{
+    return (<p>this is model</p>)
+
+}
+export default Model;
